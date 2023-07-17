eda21e147c6b3b041cd604d6b2805efa68c4337a
Mario Anthony <marioapereirar@gmail.com>
2023-07-17 19:06:07 -0300

    Finalização do desafio

--- README.md
+++ README.md
@@ -1,21 +1,127 @@
-# Meteora 
+<div align="center">
+  
+# Alura Challenge Front-end 7 edição
 
-Meteora é um modelo de ecommerce desenvolvido durante o Sétimo Challenge Front-end da Alura
+ O desafio propõe criar a página da Meteora, um ecommerce de roupas casuais e para todos os gêneros. E é uma empresa
+que busca sempre o que a de novo na moda, agora busca alcançar o mercado digital criando uma plataforma online de 
+vendas para os seus produtos.
 
-| :placard: Vitrine.Dev |     |
+| :placard: Vitrine.Dev |  [Minha Vitrine Dev](https://cursos.alura.com.br/vitrinedev/marioapereirar)   |
 | -------------  | --- |
 | :sparkles: Nome        | **Meteora**
-| :label: Tecnologias | JavaScript, html, css
-| :rocket: URL         | https://meteora-rosy.vercel.app/#
-| :fire: Desafio     | https://www.alura.com.br/challenges/front-end-7?host=https://cursos.alura.com.br
+| :label: Tecnologias |   <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"><img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"><img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+| 🎇: Bibliotecas | [Bootstrap](https://getbootstrap.com/), [SwiperApi](https://swiperjs.com/).
+| :rocket: URL         | [Projeto](https://meteora-rosy.vercel.app/)
+| :fire: Desafio     | [Conheça o Challenge Alura](https://www.alura.com.br/challenges/front-end-6?host=https://cursos.alura.com.br)
+| :page_with_curl: Licença         | [<img src="https://img.shields.io/badge/LICENSE-MIT-green"/>](https://choosealicense.com/licenses/mit/) 
+  
+</div>
 
-<!-- Inserir imagem com a #vitrinedev ao final do link -->
-![](assets/print.png#vitrinedev)
+# 💪 Sobre o Challenge da Alura
 
-## Detalhes do projeto
+É um desafio de Frontend proposto pela escola de tecnologia Alura, onde recebemos um layout completo da página no
+Figma e passos para seguir na plataforma do Trello, e que foram divididos em um período de **4 semanas**.
 
-O desafio até o momento foi só fazer a estrutura da página, o HTML e CSS.
+## Objetivo
 
-Dividindo em partes, começando pelo cabeçalho e depois o banner e assim por diante. E no final fazer a responsividade da página.
+Ao final desse desafio teremos eses conteudos prontos:
 
-Esse desafio está sendo uma grande oportunidade de colocar em prática as coisas que eu aprendi durante as minhas aulas na Alura.
+- Uma página inicial que contenha um buscador de produtos oferecidos pela empresa;
+- Um banner/carrossel que contenha informações sobre a coleção do momento;
+- Uma galeria com as categorias dos produtos disponíveis na loja;
+- Uma galeria com as informações dos produtos, como: imagem, nome do produto, descrição e link que leva até a página de “Saiba mais sobre o produto”.
+- Uma modal que contenha a imagem, o nome, a descrição, o preço, as variações do produto e a opção de adicionar na sacola;
+- Uma seção para divulgar as facilidades que a loja proporciona para realizar as compras;
+- Uma seção para cadastrar na newsletter e receber novidades;
+- Um rodapé com informação do site.
+
+
+# :one: Primeira Semana | **:white_check_mark: Feito**
+
+Esses são os meus objetivos da semana: 
+
+- :white_check_mark: Passo 1 | Cabeçalho 
+- :white_check_mark: Passo 2 | Cabecalho Responsividade
+- :white_check_mark: Passo 3 | Banner 
+- :white_check_mark: Passo 4 | Seção "Busque por categorias"
+- :white_check_mark: Passo 5 | Seção "Facilidades"
+- :white_check_mark: Passo 6 | Seção "Produtos"
+- :white_check_mark: Passo 7 | Seções "Responsividade"
+
+## Resultados da primeira semana:
+
+![Resultados da primeira semana](assets/print.png)
+## Resumo da semana:
+
+Na primeira semana não tive tantos desafios, já que nós tínhamos que fazer apenas o  HTML e CSS. E essas são áreas
+das quais eu já me dou bem. Eu optei em começar o projeto usando o método de MobileFirst por achar que depois a 
+responsividade seria mais tranquila de fazer.
+
+# :two: Segunda Semana **:white_check_mark: Feito**
+
+Esses são os meus objetivos da semana: 
+
+- :white_check_mark: Passo 1 | Rodapé
+- :white_check_mark: Passo 2 | Cadastro na Newsletter
+- :white_check_mark: Passo 3 | Carrosel
+- :white_check_mark: Passo 4 | Modal - Produtos
+- :white_check_mark: Passo 5 | Modal - Produtos responsividade
+- :white_check_mark: Passo 6 | Modal - Newslatter
+
+## Resumo da semana: 
+
+Na segunda semana foram feitos os modais, tanto dos produtos quanto do Newsletter. E aqui eu tive meu primeiro 
+desafio, porque foi a primeira vez que eu fiz um modal ou um elemento HTML que sumir e fechar na tela, e com um artigo 
+que a própria Alura disponibilizou eu acabei descobrindo a tag <dialog>, que é usada exatamente para esse tipo de 
+situação. E no final acabei conseguindo fazer todos os passos dentro do prazo da semana.
+
+# :three: e :four: Terceira e quarta semana  **:white_check_mark: Feito**
+
+Esses são os meus objetivos da semana: 
+
+- :white_check_mark: Passo 1 | Galeria de Produtos - Listagem via API
+- :white_check_mark: Passo 2 | Galeria de Produtos - Filtrar por categoria
+- :white_check_mark: Passo 3 | Galeria de Produtos - Filtrar por barra de pesquisa
+- :white_check_mark: Passo 4 | Modal de Produtos - Abrir / Fechar
+- :white_check_mark: Passo 5 | Modal de Newsletter - Abrir / Fechar
+- :white_check_mark: Passo 6 | Cadastro Newsletter - Validação
+
+
+##  Resumo da semana:
+
+Na terceira e quarta semanas foram feitas a parte lógica da página. Nos primeiros 3 passos das semanas, eu me sai bem, 
+porque eram conteúdos dos quais eu tinha acabado de estudar e tava colocando em prática como simular e tratar uma API 
+e fazer funções assincronas para manipular o DOM. No entanto, nos próximos passos eu tive um sério problema, 
+principalmente na hora de fazer a função de abrir e fechar os modais, passei um bom tempo tentando de várias formas, 
+até pedi ajuda a amigos da área. E por fim lendo alguns artigos sobre, eu optei por usar a biblioteca do Bootstrap que 
+já tem uma variedade de modais já prontos para usar e implementar no projeto. E assim consegui concluir o desafio
+
+# 🤯 Minha experiência
+
+Esse foi o primeiro desafio de Frontend da Alura em que eu participei, e tive uma experiência incrível sobre muitas
+coisas. 
+
+Com esse desafio eu pude colocar em prática os conhecimentos que consegui nos cursos da Alura, e pude ver uma melhoria 
+nas minhas habilidades, mesmo sabendo que ainda não cheguei onde quero, sinto que estou melhor.
+
+E graças ao desafio eu comecei a interagir mais com as pessoas da área, até na comunidade do próprio desafio. O que 
+antes era um verdadeiro desafio para mim, acabou que consegui conversar bem com as pessoas e até ajudar algumas.
+
+Estou satisfeito com a minha primeira participação em um desafio de desenvolvimento, terminei ele consolidando alguns 
+conhecimentos e aprendendo coisas novas, consegui interagir com algumas pessoas e superei esse desafio. Pretendo 
+participar dos próximos com toda certeza, sei que ainda não estou no meu melhor e posso melhorar bem mais esse código 
+que fiz e pretendo fazer isso.
+
+## ⭐ Representação do projeto em diversas telas
+
+<div align="center">
+![Demonstração](<video src="assets/2023-07-17%2018-46-36.mp4" controls title="Title"></video>)
+</div>
+
+# 🙋‍♂️ Autor
+
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/DanielBarret0/codeChella/blob/main/LICENSE.md)
+
+Mário Anthony Pereira Rocha
+
+Acesse minha redes [aqui](https://social-links-v2.bohr.io/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,84 @@ function criaCard(imagemMobile,imagemTablet, imagemDesktop, nome, descricao, pre
                     <h3 class="card__titulo">${nome}</h3>
                     <p class="card__descricao">${descricao}</p>
                     <p class="card__preco">R$ ${preco}</p>
-                    <button class="card__botao" id="${id}">Ver mais</button>
-                </div>`
+                    <button type="button" class="btn btn-primary card__botao" data-bs-toggle="modal" data-bs-target="#exampleModal${id}">
+                    Ver mais
+                  </button>
+                </div>
+
+<div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header modal__contato--preto">
+            <img src="./assets/Mobile/Ícones/check-circle.svg" alt="">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Confira detalhes sobre o produto</h1>
+              <button type="button" class="btn-close modal__contato--fechar" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal__produto--conteudo">
+        <div class="modal__imagem--caixa">
+            <img src="${imagemMobile}" alt="" class="modal__imagem--mobile">
+            <img src="${imagemDesktop}" alt="" class="modal__imagem--dekstop">
+        </div>
+        <div class="modal__informacoes">
+            <h3  class="modal__produto--nome">${nome}</h3>
+            <p  class="modal__produto--descricao">${descricao}</p>
+            
+            <div class="modal__produto--linha"></div>
+            <p  class="modal__produto--preco">R$ ${preco}</p>
+            <p  class="modal__produto--loja">Vendido e entregue por Riachuelo</p>
+    
+            <hr>
+            <P class="modal__produto--legenda">Cores:</P>
+            <form class="modal__produto--formulario">
+                <div>
+                    <input type="radio" name="azul-claro" id="">
+                    <label for="azul-claro">Azul claro</label>
+                </div>
+                <div>
+                    <input type="radio" name="offwhite" id="">
+                    <label for="offwhite">Offwhite</label>
+                </div>
+                <div>
+                    <input type="radio" name="preto" id="">
+                    <label for="preto">Preto</label>
+                </div>
+            </form>
+    
+            <hr>
+             <P class="modal__produto--legenda">Tamanho:</P>
+            <form action=""  class="modal__produto--formulario">
+    
+                <div>
+                    <input type="radio" name="pp" id="">
+                    <label for="pp">PP</label>
+                </div>
+    
+                <div>
+                    <input type="radio" name="p" id="">
+                    <label for="p">P</label>
+                </div>
+    
+                <div>
+                    <input type="radio" name="m" id="">
+                    <label for="m">M</label>
+                </div>
+    
+                <div>
+                    <input type="radio" name="g" id="">
+                    <label for="g">G</label>
+                </div>
+    
+                <div>
+                    <input type="radio" name="gg" id="">
+                    <label for="gg">GG</label>
+                </div>
+            </form>
+            <button type="button" class="btn btn-primary modal__produto--botao">Adicionar à sacola</button>
+        </div>
+               
+          </div>
+        </div>
+      </div>`
 
     return produto
 }
@@ -78,102 +154,31 @@ botaoPesquisaDesktop.addEventListener('click', evento => buscaProduto(evento));
 
 // MODAL
 
-const botaoModal = document.querySelector(".card__botao");
-const sectionModal = document.getElementById("section__modal")
-
-async function chamaModal(){
-    const elementoBotao = document.getElementById(this.id)
-    const id = elementoBotao.id;
-    const lista = await conectaApi.listaProdutos();
-     const produtosFiltrados = lista.filter(produto => produto.id == id)
-
-     sectionModal.innerHTML = ''
-     produtosFiltrados.forEach(elemento => {
-            sectionModal.appendChild(criaModal(elemento.imagemMobile, elemento.imagemDesktop, elemento.nome, elemento.descricao, elemento.preco).showModal())
-        })
 
 
+
+// MODAL NEWSLETTER
+
+const botaoNewsletter = document.querySelector('.contato__botao');
+const modalNewsletter = document.querySelector('.modal__contato');
+const fecharModalNewsletter = document.getElementById('modal__contato--fechar');
+const inputNewsletter = document.querySelector(".contato__input");
+const newsletterErro = document.querySelector(".contato__erro");
+
+function chamaModalNewsletter(e) {
+    e.preventDefault()
+    if(inputNewsletter.value = ''){
+        newsletterErro.innerHTML = "O campo de e-mail não pode estar vazio!"
+        return
+    }
+    if(inputNewsletter.value.length < 5){
+        newsletterErro.innerHTML = "Seu e-mail é inválido!"
+        return
+    }
+    modalNewsletter.showModal()
+    newsletterErro.innerHTML = ''
 }
 
+botaoNewsletter.addEventListener('click', e => chamaModalNewsletter(e));
 
-botaoModal.addEventListener('click', chamaModal);
-
-console.log(botaoModal);
-
-function criaModal(imagemMobile, imagemDesktop, nome, descricao, preco) {
-    const produto = document.createElement('div');
-    produto.className = "novidades__card";
-
-    produto.innerHTML = `<dialog class="modal__produto">
-    <div class="modal__contato--preto">
-        <img src="./assets/Mobile/Ícones/check-circle.svg" alt="">
-        <p>Confira detalhes sobre o produto</p>
-        <button class="modal__contato--fechar"></button>
-    </div>
-    <div class="modal__produto--conteudo">
-    <div>
-        <img src="${imagemMobile}" alt="" class="modal__imagem--mobile">
-        <img src="${imagemDesktop}" alt="" class="modal__imagem--dekstop">
-    </div>
-    <div class="modal__informacoes">
-        <h3  class="modal__produto--nome">${nome}</h3>
-        <p  class="modal__produto--descricao">${descricao}</p>
-        
-        <div class="modal__produto--linha"></div>
-        <p  class="modal__produto--preco">R$ ${preco}</p>
-        <p  class="modal__produto--loja">Vendido e entregue por Riachuelo</p>
-
-        <hr>
-        <P class="modal__produto--legenda">Cores:</P>
-        <form class="modal__produto--formulario">
-            <div>
-                <input type="radio" name="azul-claro" id="">
-                <label for="azul-claro">Azul claro</label>
-            </div>
-            <div>
-                <input type="radio" name="offwhite" id="">
-                <label for="offwhite">Offwhite</label>
-            </div>
-            <div>
-                <input type="radio" name="preto" id="">
-                <label for="preto">Preto</label>
-            </div>
-        </form>
-
-        <hr>
-         <P class="modal__produto--legenda">Tamanho:</P>
-        <form action=""  class="modal__produto--formulario">
-
-            <div>
-                <input type="radio" name="pp" id="">
-                <label for="pp">PP</label>
-            </div>
-
-            <div>
-                <input type="radio" name="p" id="">
-                <label for="p">P</label>
-            </div>
-
-            <div>
-                <input type="radio" name="m" id="">
-                <label for="m">M</label>
-            </div>
-
-            <div>
-                <input type="radio" name="g" id="">
-                <label for="g">G</label>
-            </div>
-
-            <div>
-                <input type="radio" name="gg" id="">
-                <label for="gg">GG</label>
-            </div>
-        </form>
-        <button class="modal__produto--botao">Adicionar à sacola</button>
-    </div>
-</div>
-</dialog>
-`
-
-    return produto
-}
+fecharModalNewsletter.addEventListener('click', () => modalNewsletter.close());
